dd199f6613f31e53992ffb31ac8e1bc85f3da9e9
czishibashi-genki <ishibashi-genki@cyber-z.co.jp>
2016-10-16 15:10:13 +0900

    add commnent

--- GCSImageClient.py
+++ GCSImageClient.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Google画像検索api←2011年に廃止。その代わりとしてGoogle custom search apiというものを利用している。
+Google custom search(以下:GCS)とは以下のことが可能である
+* 自分のサイトにグーグル検索窓を作ることができる
+* 画像検索も可能
+
+■ GCSの画像検索の仕様
+└ ドメイン or URLを指定
+└ 指定したサイトに関連する情報・画像が取得
+
+ただし、全ドメインをしてもGoogleと同じ検索結果にはならない
+https://support.google.com/customsearch//answer/2630972?hl=ja&ctx=topic&topic=2568895
+
+TODO:画像のメタ情報取得(幅、高さ、画像サイズ)
+"""
 
 import urllib2
 import json
 import domain
 from itertools import chain
 
-BASE_URL = 'https://www.googleapis.com/customsearch/v1'#?key=AIzaSyA1n6G5_ZDAXyEQpkRO-MpPwGWSYDvYVoU&cx=017028400548203066578:uuq01vmwwue'
+BASE_URL = 'https://www.googleapis.com/customsearch/v1'
 API_KEY = 'AIzaSyA1n6G5_ZDAXyEQpkRO-MpPwGWSYDvYVoU'
 ENGINE_ID = '017028400548203066578:uuq01vmwwue'
 INTEGRATED_ID = 3
